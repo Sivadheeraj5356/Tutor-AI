24073c3951a6750b08c32b8a283e278b81d588b5
Sivadheeraj5356 <sivadheerajethamsetti@gmail.com>
2025-02-05 18:42:42 +0530

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,27 +1,37 @@
 import { assets } from "../assets/assets"
+import { useState } from "react"
 export function Sidebar(){
-   return <div className="max-sm:hidden min-h-screen w-56 bg-slate-100 flex flex-col justify-between dark:bg-neutral-800" >
+  const [fullSidebar, setFullSidebar] = useState(true)
+   return <div className={`max-sm:hidden min-h-screen ${ fullSidebar ? 'w-56' : 'w-20'} bg-slate-100 flex flex-col justify-between dark:bg-neutral-800 `} id="sidebar" >
     <div>
-      <img className={"h-12 w-12 pt-5 pl-5 cursor-pointer dark:invert"} src={assets.menu_icon} alt="" />
+      <img className={"h-12 w-12 pt-5 pl-5 cursor-pointer dark:invert"} src={assets.menu_icon} alt=""
+      onClick={()=>setFullSidebar(prev => !prev)}
+      />
+     {fullSidebar ? <>
       <div className="flex justify-center gap-2 items-center mt-10  pr-3 bg-white dark:bg-neutral-800 shadow-lg rounded-full h-12 w-36 pl-5 ml-5 cursor-pointer">
         <img className={"h-8 w-6 dark:invert"} src={assets.plus_icon} alt="" />
         <div className="w-20 dark:text-white">New Chat</div>
       </div>
+     </> : 
+     <>
+      <div className="flex justify-center items-center mt-10 bg-white dark:bg-neutral-800 shadow-lg rounded-full h-10 w-10 ml-4 cursor-pointer">
+        <img className={"h-8 w-6 dark:invert"} src={assets.plus_icon} alt="" />
+      </div></>}
 
-      <div className="ml-5 mt-10 font-semibold dark:text-white">Recent</div>
+     {fullSidebar && <div className="ml-5 mt-10 font-semibold dark:text-white">Recent</div> } 
       </div>
       <div className="flex flex-col gap-6 ml-5 mb-10">
         <div className="grid grid-cols-4 gap-3 cursor-pointer"> 
         <img className="h-6 w-6 col-span-1 dark:invert" src={assets.question_icon} alt="" />
-        <div className="col-span-2 dark:text-white">Help</div>
+       {fullSidebar && <div className="col-span-2 dark:text-white">Help</div> } 
         </div>
         <div className="grid grid-cols-4 gap-2 cursor-pointer">
         <img className="h-6 w-6 col-span-1 dark:invert" src={assets.history_icon} alt="" />
-        <div className="col-span-2 dark:text-white">Activity</div>
+        {fullSidebar && <div className="col-span-2 dark:text-white">Activity</div> } 
         </div>
         <div className="grid grid-cols-4 gap-1 cursor-pointer">
         <img className="h-6 w-6 cols-span-1 dark:invert" src={assets.setting_icon} alt="" />
-        <div className="col-span-2 dark:text-white">Settings</div>
+        {fullSidebar && <div className="col-span-2 dark:text-white">Settings</div> } 
         </div>
       </div>
    </div>
